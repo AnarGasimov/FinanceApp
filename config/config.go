5733fee002d75a9d0344f35ce066cb0daab72f6d
Anar <anar.gasimov@yahoo.com>
2025-04-24 18:31:08 +0400

    run command fixed

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ func LoadConfig() (*Config, error) {
 		".",                      // current directory
 		"..",                     // parent directory
 		"../..",                  // two levels up
-		"../../..",                  // three levels up
+		"./FinanceApp/config",		
 		os.Getenv("CONFIG_PATH"), // environment variable to specify path
 	}
 
